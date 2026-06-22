acbb2dce9758cf420dc129a91fab4632cb85580d
Dmitriy Zaikin <finnikisafruit@gmail.com>
2026-06-23 01:01:37 +0300

    refactoring, add form login, fixed models

---
+++ webapp/__init__.py
@@ -0,0 +1,47 @@
+from pathlib import Path
+
+from flask import Flask, render_template
+
+from webapp.forms import LoginForm
+
+app = Flask(__name__)
+app.config.from_pyfile('config.py')
+# @login_manager.user_loader
+# def load_user(user_id):
+#    return db_session.get(User, int(user_id))
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/login')
+def login():
+    title = 'Авторизация'
+    login_form = LoginForm()
+    return render_template('login.html', page_title=title, form=login_form)
+
+
+# @app.route('/user')
+# def user():
+#    return render_template('user.html')
+
+
+# @app.route('/basket')
+# def basket():
+#    return render_template('basket.html')
+
+
+# @app.route('/my_announcement')
+# def my_announcement():
+#    return render_template('my_announcement.html')
+
+
+# app.route('/create_announcement')
+# def create_announcement():
+#    return render_template('create_announcement.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ webapp/models.py
@@ -0,0 +1,47 @@
+from flask_login import UserMixin
+from sqlalchemy import ForeignKey, String
+from sqlalchemy.orm import Mapped, mapped_column
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from db import Base, engine
+
+
+class User(UserMixin, Base):
+    __tablename__ = 'users'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    login: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    password: Mapped[str] = mapped_column(String(120), nullable=False)
+    role: Mapped[str] = mapped_column(String(120), index=True, default='user')
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
+    def is_admin(self):
+        return self.role == 'admin'
+
+    def __repr__(self):
+        return f'<User id: {self.id}, User login: {self.login}>'
+
+
+class Product(Base):
+    __tablename__ = 'products'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey('users.id'))
+    title: Mapped[str] = mapped_column(String(120))
+    size: Mapped[str] = mapped_column(String(20), nullable=False)
+    price: Mapped[int] = mapped_column(nullable=False)
+    description: Mapped[str] = mapped_column(String(300))
+    url: Mapped[str] = mapped_column(String(300))
+    city: Mapped[str] = mapped_column(String(120))
+    condition: Mapped[str] = mapped_column(String(120))
+
+    def __repr__(self):
+        return f'<Product id: {self.id}, {self.title}>'
+
+
+if __name__ == '__main__':
+    Base.metadata.create_all(engine)
