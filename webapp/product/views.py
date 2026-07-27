3e7583822ca5e6e3ba61e329b8bc58170f4e5c0f
Dmitriy Zaikin <finnikisafruit@gmail.com>
2026-07-27 23:52:19 +0300

    add aedit comments

--- webapp/product/views.py
+++ webapp/product/views.py
@@ -94,6 +94,46 @@ def edit_product(product_id):
     )
 
 
+@blueprint.route('/process-edit-product/<int:product_id>', methods=['POST'])
+@login_required
+def process_edit_product(product_id):
+    product = db_session.get(Product, product_id)
+
+    if product is None:
+        flash('Товар не найден')
+        return redirect(url_for('main_page.index'))
+    if product.user_id != current_user.id:
+        flash('Вы не можете редактировать чужой товар')
+        return redirect(url_for('product.product_page', product_id=product_id))
+
+    form = EditProductForm()
+
+    if form.validate_on_submit():
+        product.title = form.title.data
+        product.size = str(form.size.data)
+        product.price = form.price.data
+        product.description = form.description.data
+        product.city = form.city.data
+        product.condition = form.condition.data
+        if form.image.data:
+            file = form.image.data
+            filename = secure_filename(file.filename)
+            unique_name = f'{uuid.uuid4().hex}_{filename}'
+            file.save(os.path.join(UPLOAD_FOLDER, unique_name))
+            product.image_filename = unique_name
+        db_session.commit()
+
+        flash('Вы успешно изменили данные')
+        return redirect(url_for('product.product_page', product_id=product.id))
+
+    return render_template(
+        'product/edit_product.html',
+        page_title='Редактирование товара',
+        form=form,
+        product=product,
+    )
+
+
 @blueprint.route('/process-add-comment/<int:product_id>', methods=['POST'])
 @login_required
 def process_add_comment(product_id):
@@ -121,11 +161,74 @@ def process_add_comment(product_id):
         flash('Комментарий не добавлен')
         return redirect(url_for('product.product_page', product_id=product.id))
 
+
+@blueprint.route('/process-delete-comment/<int:comment_id>', methods=['POST'])
+@login_required
+def process_delete_comment(comment_id):
+    comment = db_session.get(Comment, comment_id)
+
+    if comment is None:
+        flash('Комментарий не найден')
+        return redirect(url_for('main_page.index'))
+
+    if comment.user_id != current_user.id:
+        flash('Вы можете удалить только свой комментарий')
+        return redirect(url_for('product.product_page', product_id=comment.product.id))
+
+    product_id = comment.product_id
+    db_session.delete(comment)
+    db_session.commit()
+    flash('Комментарий удален')
+    return redirect(url_for('product.product_page', product_id=product_id))
+
+
+@blueprint.route('/edit_comment/<int:comment_id>')
+@login_required
+def edit_comment(comment_id):
+    comment = db_session.get(Comment, comment_id)
+
+    if comment is None:
+        flash('Комментарий не найден')
+        return redirect(url_for('main_page.index'))
+    if comment.user_id != current_user.id:
+        flash('Вы можете редактировать только свой комментарий')
+        return redirect(url_for('product.product_page', product_id=comment.product_id))
+
+    form = CommentForm()
+    form.text.data = comment.text
     return render_template(
-        'product/edit_product.html',
-        page_title='Редактирование товара',
+        'product/edit_comment.html',
+        page_title='редактированиек комментария',
         form=form,
-        product=product,
+        comment=comment,
+    )
+
+
+@blueprint.route('/process-edit-comment/<int:comment_id>', methods=['POST'])
+@login_required
+def process_edit_comment(comment_id):
+    comment = db_session.get(Comment, comment_id)
+
+    if comment is None:
+        flash('Комментарий не найден')
+        return redirect(url_for('main_page.index'))
+    if comment.user_id != current_user.id:
+        flash('Вы можете редактировать только свой комментарий')
+        return redirect(url_for('product.product_page', product_id=comment.product_id))
+
+    form = CommentForm()
+
+    if form.validate_on_submit():
+        comment.text = form.text.data
+        db_session.commit()
+        flash('Вы успешно изменили комментарий')
+        return redirect(url_for('product.product_page', product_id=comment.product.id))
+
+    return render_template(
+        'product/edit_comment.html',
+        page_title='Редактирование комментария',
+        form=form,
+        comment=comment,
     )
 
 
